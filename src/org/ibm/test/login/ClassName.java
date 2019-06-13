6b945ae051ff7feb6fb6a9d917496a8b7821f60c
asar12 <asar.softies@gmail.com>
2019-06-13 09:10:04 +0530

    new third

---
+++ src/org/ibm/test/login/ClassName.java
@@ -0,0 +1,5 @@
+package org.ibm.test.login;
+
+public class ClassName {
+
+}
---
+++ src/org/ibm/test/login/Str.java
@@ -0,0 +1,8 @@
+package org.ibm.test.login;
+
+public class Str {
+public static void main(Str[] args) {
+	String s= "welcome to india";
+	System.out.println(s.length());
+}
+}
